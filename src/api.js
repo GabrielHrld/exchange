42ac0768668c62472744cef86a72476c46f9699e
Gabi <g.federico.rodriguez@gmail.com>
2021-01-17 19:21:26 -0300

    coin detail implemented

--- src/api.js
+++ src/api.js
@@ -11,6 +11,26 @@ const getAssets = () => {
     });
 };
 
+const getAsset = (coin) => {
+  return fetch(`https://api.coincap.io/v2/assets/${coin}`)
+  .then(res => res.json())
+  .then(res => res.data)
+}
+
+const getAssetHistory = (coin) =>{
+
+  const now = new Date()
+  const end = now.getTime()
+  now.setDate(now.getDate() - 1)
+  const start = now.getTime()
+
+  return fetch(`https://api.coincap.io/v2/assets/${coin}/history?interval=h1&start=${start}&end=${end}`)
+  .then(res => res.json())
+  .then(res => res.data)
+}
+
 export default {
   getAssets,
+  getAsset,
+  getAssetHistory
 };
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Router from "vue-router";
 import Home from "@/views/Home";
 import About from "@/views/About";
 import Error from "@/views/Error";
+import CoinDetail from '@/views/CoinDetail'
 
 Vue.use(Router);
 
@@ -22,9 +23,15 @@ export default new Router({
       component: About,
     },
     {
+      path: '/coin/:id',
+      name: 'coin-detail',
+      component: CoinDetail
+    },
+    {
       path: "/*",
       name: "error",
       component: Error,
     },
+    
   ],
 });
