1e425b7b593e36550193bdbe45f52f6c8b8e88c6
Gabi <g.federico.rodriguez@gmail.com>
2021-01-17 14:55:02 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main></main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PxHeader from "@/components/PxHeader";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { PxHeader},
+};
 </script>
 
 <style>
